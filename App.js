d3efbaa3c77fea790c662ecbc2a3934cfb9793ad
ElReyAlfredo <jdelgado7@ucol.mx>
2023-11-09 11:34:26 -0600

    first commit

--- App.js
+++ App.js
@@ -1,132 +1,17 @@
 import React, { Component } from "react";
-import {
-  Button,
-  Switch,
-  StyleSheet,
-  TextInput,
-  Alert,
-  Text,
-  View,
-  Image,
-} from "react-native";
-import { StatusBar } from "expo-status-bar";
+import loginView from "./src/LoginView";
+import registerView from "./src/registerView";
+import { Actions, Scene, Router } from "react-native-router-flux";
+
+const scenes = Actions.create(
+  <Scene key="root">
+    <Scene key="login" component={loginView} title="Login" hideNavBar />
+    <Scene key="register" component={registerView} title="Register" />
+  </Scene>
+);
 
 export default class App extends Component {
-  state = {
-    email: "",
-    password: "",
-    isValidEmail: true,
-    isValidPassword: true,
-  };
-
-  handleEmailChange = (text) => {
-    this.setState({ email: text });
-    const regex = /^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const isValidEmail = regex.test(text);
-    this.setState({ isValidEmail });
-  };
-
-  handlePasswordChange = (text) => {
-    this.setState({ password: text });
-    const isValidPassword = text.length >= 8;
-    this.setState({ isValidPassword });
-  };
-
-  handleSubmit = () => {
-    if (!this.state.isValidEmail) {
-      Alert.alert("Invalid Email", "Please enter a valid email address.");
-      return;
-    }
-
-    if (!this.state.isValidPassword) {
-      Alert.alert(
-        "Invalid Password",
-        "Password must be at least 8 characters long."
-      );
-      return;
-    }
-
-    Alert.alert("Successful Login", "You have been logged in successfully!");
-  };
-
   render() {
-    const { email, password, isValidEmail, isValidPassword } = this.state;
-    return (
-      <View style={styles.container}>
-        <Image style={styles.logo} source={require("./assets/p1.jpg")} />
-        <Text style={styles.subTitle}>Sign In to your account</Text>
-        <TextInput
-          placeholder="email@email.com"
-          style={[
-            styles.textInput,
-            isValidEmail ? styles.validInput : styles.invalidInput,
-          ]}
-          onChangeText={this.handleEmailChange}
-          value={email}
-        />
-        <TextInput
-          placeholder="password"
-          style={[
-            styles.textInput,
-            isValidPassword ? styles.validInput : styles.invalidInput,
-          ]}
-          onChangeText={this.handlePasswordChange}
-          value={password}
-          secureTextEntry={true}
-        />
-        <View style={styles.buttonContainer}>
-          <Button
-            title="Log In"
-            onPress={this.handleSubmit}
-            style={styles.loginButton}
-            //disabled={!isValidEmail || !isValidPassword}
-          />
-        </View>
-        <StatusBar style="auto" />
-      </View>
-    );
+    return <Router scenes={scenes} />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  titulo: {
-    fontSize: 80,
-    color: "#000",
-    fontWeight: "bold",
-  },
-  subTitle: {
-    fontSize: 20,
-    color: "gray",
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "gray",
-    padding: 10,
-    width: "80%",
-    marginTop: 20,
-    borderRadius: 30,
-  },
-  validInput: {
-    borderColor: "green",
-  },
-  invalidInput: {
-    borderColor: "red",
-  },
-  buttonContainer: {
-    marginTop: 20,
-    width: "50%",
-  },
-  loginButton: {
-    width: "100%", // Establece la anchura del botón
-  },
-  logo: {
-    width: 200, // Ancho deseado
-    height: 100, // Alto deseado
-  },
-});
--- src/registerView.js
+++ src/registerView.js
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { Component } from "react";
 import {
@@ -7,20 +8,36 @@ import {
   Button,
   TextInput,
   Alert,
-  Image,
+  TouchableOpacity,
 } from "react-native";
 import { Actions } from "react-native-router-flux";
 
-export default class loginView extends Component {
+export default class registerView extends Component {
   constructor(props) {
     super(props);
     this.state = {
       email: "",
       password: "",
+      confirmPassword: "",
       isValidEmail: true,
       isValidPassword: true,
+      userName: "",
     };
   }
+
+  static navigationOptions = {
+    title: "Registro",
+    headerLeft: () => (
+      <TouchableOpacity onPress={() => Actions.pop()}>
+        <Text style={{ marginLeft: 20, marginRight: 50 }}>Volver</Text>
+      </TouchableOpacity>
+    ),
+  };
+
+  handleGoBack = () => {
+    const { navigation } = this.props;
+    navigation.goBack();
+  };
 
   handleEmailChange = (text) => {
     this.setState({ email: text });
@@ -34,6 +51,19 @@ export default class loginView extends Component {
     const valid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9\s])\S{8,}$/;
     const isValidPassword = valid.test(text);
     this.setState({ isValidPassword });
+  };
+
+  handleConfirmPasswordChange = (text) => {
+    this.setState({ confirmPassword: text });
+    const valid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9\s])\S{8,}$/;
+    const isValidPasswordConfirmation = valid.test(text);
+    this.setState({ isValidPasswordConfirmation });
+  };
+
+  handleUserChange = (text) => {
+    this.setState({ userName: text });
+    const isValidUser = text.length >= 4;
+    this.setState({ isValidUser });
   };
 
   handleSubmit = () => {
@@ -63,31 +93,69 @@ export default class loginView extends Component {
       return;
     }
 
-    Alert.alert("Successful Login", "You have been logged in successfully!");
+    if (!this.state.isValidUser) {
+      Alert.alert(
+        "Invalid Username",
+        "Username must be at least 4 characters long."
+      );
+      return;
+    }
+
+    // Check if passwords match
+    if (this.state.password !== this.state.confirmPassword) {
+      Alert.alert(
+        "Passwords do not match",
+        "Please enter the same password in both fields."
+      );
+      return;
+    }
+
+    // Passwords match, proceed with registration
+    Alert.alert(
+      "Successful Registration",
+      "You have been registered successfully!"
+    );
+    Actions.login();
   };
 
+  /*
   register = () => {
-    Actions.register();
+    Actions.login();
   };
+  */
 
   render() {
     return (
       <View style={styles.container}>
-        <Image style={styles.logo} source={require("./assets/p1.jpg")}></Image>
-        <Text style={styles.subTitle}>Sign In to your account</Text>
+        <Text style={styles.subTitle}>Sign Up to your account</Text>
+
         <TextInput
           style={styles.textInput}
           onChangeText={this.handleEmailChange}
           value={this.state.email}
-          placeholder="email"
+          placeholder="Create an email"
           keyboardType="email-address"
         />
         <TextInput
-          placeholder="password"
+          style={styles.textInput}
+          onChangeText={this.handleUserChange}
+          value={this.state.userName}
+          placeholder="Username"
+          keyboardType="default"
+        />
+        <TextInput
+          placeholder="Create a password"
           style={styles.textInput}
           secureTextEntry={true}
           onChangeText={this.handlePasswordChange}
           value={this.state.password}
+        />
+        <TextInput
+          placeholder="Confirm password"
+          style={styles.textInput}
+          secureTextEntry={true}
+          onChangeText={this.handleConfirmPasswordChange}
+          value={this.state.confirmPassword}
         />
         <View style={styles.buttonContainer}>
           <Button
@@ -97,13 +165,7 @@ export default class loginView extends Component {
             accessibilityLabel="Learn more about this purple button"
           />
         </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            style={styles.loginButtonbutton}
-            onPress={this.register}
-            title="Crear una cuenta"
-          />
-        </View>
+
         <StatusBar style="auto" />
       </View>
     );
@@ -116,10 +178,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  logo: {
-    width: 200, // Ancho deseado
-    height: 100, // Alto deseado
   },
   errorText: {
     color: "#A93226",
